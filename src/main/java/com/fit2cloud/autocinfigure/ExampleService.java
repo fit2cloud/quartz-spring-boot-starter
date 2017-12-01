5aaf4197af49fbbb7ea70006a040401bae5c0536
Captain.B <bin@fit2cloud.com>
2017-12-01 23:05:45 +0800

    init

---
+++ src/main/java/com/fit2cloud/autocinfigure/ExampleService.java
@@ -0,0 +1,15 @@
+package com.fit2cloud.autocinfigure;
+
+public class ExampleService {
+
+    private String prefix;
+    private String suffix;
+
+    public ExampleService(String prefix, String suffix) {
+        this.prefix = prefix;
+        this.suffix = suffix;
+    }
+    public String wrap(String word) {
+        return prefix + word + suffix;
+    }
+}
---
+++ src/main/java/com/fit2cloud/autocinfigure/ExampleServiceProperties.java
@@ -0,0 +1,23 @@
+package com.fit2cloud.autocinfigure;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+@ConfigurationProperties("example.service")
+public class ExampleServiceProperties {
+    private String prefix;
+    private String suffix;
+
+    public String getPrefix() {
+        return prefix;
+    }
+
+    public void setPrefix(String prefix) {
+        this.prefix = prefix;
+    }
+
+    public String getSuffix() {
+        return suffix;
+    }
+
+    public void setSuffix(String suffix) {
+        this.suffix = suffix;
+    }
+}
